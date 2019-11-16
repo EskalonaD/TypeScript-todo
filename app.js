9288147a0e18453928f0c26f880717c8a98872a6
EskalonaD <eskalonad@gmail.com>
2019-11-16 18:51:58 +0200

    complete Type fundamentals learning

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-var todo = 'Pick up drycleaning';
-var name = 'Jess';
+//Type Fundamentals
+var animal = {
+    name: 'fido',
+    species: 'dog',
+    age: calculateAge(2014),
+    speak: function () {
+        console.log('woof!');
+    }
+};
+function calculateAge(birthYear) {
+    return Date.now() - birthYear;
+}
+function totalLength(x, y) {
+    var total = x.length + y.length;
+    x.slice(0);
+    if (x instanceof Array) {
+        x.push(1);
+    }
+    if (typeof x === 'string') {
+        x.substr(1);
+    }
+    return total;
+}
+// Custom Types
--- app.ts
+++ app.ts
@@ -1,2 +1,35 @@
-let todo: string = 'Pick up drycleaning';
-let name: string = 'Jess';
+//Type Fundamentals
+
+var animal = { 
+    name: 'fido', 
+    species: 'dog', 
+    age: calculateAge(2014), 
+    speak: function() {
+        console.log('woof!')
+    }
+}
+
+function calculateAge(birthYear) {
+    return Date.now() - birthYear
+}
+
+
+function totalLength (x: string,y: string): number
+function totalLength (x: any[], y: any[]): number
+function totalLength (x: (any[] | string),y: (any[] |string)): number {
+    let total: number = x.length + y.length;
+    x.slice(0);
+    if(x instanceof Array) {
+
+        x.push(1);
+    }
+
+    if(typeof x === 'string'){
+
+        x.substr(1)
+    }
+    return total;
+}
+
+
+// Custom Types
