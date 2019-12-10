fef33655b676d5eda4192493b8b24a7bec1e4f69
Denys Eskalona-Lopes <denys_eskalona-lopes@epam.com>
2019-12-10 13:35:15 +0200

    Complete turning TodoApp into typescript

--- TodoApp.js
+++ TodoApp.js
@@ -1,39 +1,38 @@
-function TodoApp(el, todos) {
-    this.todoService = new TodoService(todos);
-    this.initialize(el);
-}
-TodoApp.prototype.addTodo = function (todoName) {
-    this.todoService.add(todoName);
-    this.renderTodos();
-};
-TodoApp.prototype.clearCompleted = function () {
-    this.todoService.clearCompleted();
-    this.renderTodos();
-};
-TodoApp.prototype.toggleTodoState = function (todoId) {
-    this.todoService.toggle(todoId);
-    this.renderTodos();
-};
-TodoApp.prototype.renderTodos = function () {
-    var todos = this.todoService.getAll();
-    this.todoList.render(todos);
-};
-TodoApp.prototype.initialize = function (el) {
-    var _this = this;
-    var addTodoFormEl = el.getElementsByClassName('add-todo')[0], addTodoNameEl = addTodoFormEl.getElementsByTagName('input')[0], todoListEl = el.getElementsByClassName('todo-list')[0], clearCompletedEl = el.getElementsByClassName('clear-completed')[0];
-    addTodoFormEl.addEventListener('submit', function (event) {
-        _this.addTodo(addTodoNameEl.value);
-        addTodoNameEl.value = '';
-        event.preventDefault();
-    });
-    todoListEl.addEventListener('todo-toggle', function (event) {
-        var todoId = event.detail.todoId;
-        _this.todoService.toggle(todoId);
-        _this.renderTodos();
-    });
-    clearCompletedEl.addEventListener('click', function () {
-        _this.clearCompleted();
-    });
-    this.todoList = new TodoListComponent(todoListEl);
-    this.renderTodos();
-};
+var TodoApp = /** @class */ (function () {
+    function TodoApp(el, todos) {
+        this.todoService = new TodoService(todos);
+        this.initialize(el);
+    }
+    TodoApp.prototype.initialize = function (el) {
+        var _this = this;
+        var addTodoFormEl = el.getElementsByClassName('add-todo')[0], addTodoNameEl = addTodoFormEl.getElementsByTagName('input')[0], todoListEl = el.getElementsByClassName('todo-list')[0], clearCompletedEl = el.getElementsByClassName('clear-completed')[0];
+        addTodoFormEl.addEventListener('submit', function (event) {
+            _this.addTodo(addTodoNameEl.value);
+            addTodoNameEl.value = '';
+            event.preventDefault();
+        });
+        todoListEl.addEventListener('todo-toggle', function (event) {
+            var todoId = event.detail.todoId;
+            _this.todoService.toggle(todoId);
+            _this.renderTodos();
+        });
+        clearCompletedEl.addEventListener('click', function () {
+            _this.clearCompleted();
+        });
+        this.todoList = new TodoListComponent(todoListEl);
+        this.renderTodos();
+    };
+    TodoApp.prototype.renderTodos = function () {
+        var todos = this.todoService.getAll();
+        this.todoList.render(todos);
+    };
+    TodoApp.prototype.clearCompleted = function () {
+        this.todoService.clearCompleted();
+        this.renderTodos();
+    };
+    TodoApp.prototype.addTodo = function (todoName) {
+        this.todoService.add(todoName);
+        this.renderTodos();
+    };
+    return TodoApp;
+}());
--- TodoApp.ts
+++ TodoApp.ts
@@ -1,66 +1,63 @@
 interface ITodoApp {
-    clearCompleted,
-    renderTodos,
+    clearCompleted(): void,
     todoService: TodoService,
-    initialize,
-    add
-}
-// class TodoApp {
-//     constructor(el, todos)
-// }
-
-function TodoApp(el, todos) {
-
-    this.todoService = new TodoService(todos);
-    this.initialize(el);
-}
-
-TodoApp.prototype.addTodo = function (todoName) {
-    this.todoService.add(todoName);
-    this.renderTodos();
-}
-
-TodoApp.prototype.clearCompleted = function () {
-    this.todoService.clearCompleted();
-    this.renderTodos();
-}
-
-TodoApp.prototype.toggleTodoState = function (todoId) {
-    this.todoService.toggle(todoId);
-    this.renderTodos();
-}
-
-TodoApp.prototype.renderTodos = function () {
-    var todos = this.todoService.getAll();
-    this.todoList.render(todos);
-}
-
-TodoApp.prototype.initialize = function (el) {
-
-    var _this = this;
-        
-    var addTodoFormEl = el.getElementsByClassName('add-todo')[0],
-        addTodoNameEl = addTodoFormEl.getElementsByTagName('input')[0],
-        todoListEl = el.getElementsByClassName('todo-list')[0],
-        clearCompletedEl = el.getElementsByClassName('clear-completed')[0];
-
-    addTodoFormEl.addEventListener('submit', function (event) {
-        _this.addTodo(addTodoNameEl.value)
-        addTodoNameEl.value = '';
-        event.preventDefault();
-    });
-
-    todoListEl.addEventListener('todo-toggle', function (event) {
-        var todoId = event.detail.todoId;
-        _this.todoService.toggle(todoId);
-        _this.renderTodos();
-    });
-        
-    clearCompletedEl.addEventListener('click', function () {
-        _this.clearCompleted();
-    });
+    addTodo(name: string): void,
+    todoList: TodoListComponent
     
-    this.todoList = new TodoListComponent(todoListEl);
+}
+
+class TodoApp implements ITodoApp {
+    constructor(el: HTMLElement, todos: []) {
+
+        this.todoService = new TodoService(todos);
+        this.initialize(el);
+    }
+    public todoService: TodoService;
+
+    public todoList: TodoListComponent;
+
+    private initialize(el: HTMLElement) {
+        const _this = this;
+
+        const addTodoFormEl = el.getElementsByClassName('add-todo')[0],
+            addTodoNameEl = addTodoFormEl.getElementsByTagName('input')[0],
+            todoListEl = el.getElementsByClassName('todo-list')[0],
+            clearCompletedEl = el.getElementsByClassName('clear-completed')[0];
+
+        addTodoFormEl.addEventListener('submit', function (event) {
+            _this.addTodo(addTodoNameEl.value)
+            addTodoNameEl.value = '';
+            event.preventDefault();
+        });
+
+        todoListEl.addEventListener('todo-toggle', function (event) {
+            const todoId = (<CustomEvent>event).detail.todoId;
+            _this.todoService.toggle(todoId);
+            _this.renderTodos();
+        });
+
+        clearCompletedEl.addEventListener('click', function () {
+            _this.clearCompleted();
+        });
+
+        this.todoList = new TodoListComponent(todoListEl);
     
-    this.renderTodos();
+        this.renderTodos();
+    }
+
+    private renderTodos() {
+        const todos = this.todoService.getAll();
+        this.todoList.render(todos);
+    }
+
+    public clearCompleted(): void {
+        this.todoService.clearCompleted();
+        this.renderTodos();
+    }
+
+    public addTodo(todoName: string): void {
+        this.todoService.add(todoName);
+        this.renderTodos();
+    }
+
 }
